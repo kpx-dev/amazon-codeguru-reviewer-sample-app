17a01f23f2ce3f6e28e07178d9832acc9adbb831
Kien Pham <kienpham@amazon.com>
2024-08-09 14:08:47 -0700

    testing pr

--- src/main/java/com/shipmentEvents/util/S3ClientUtil.java
+++ src/main/java/com/shipmentEvents/util/S3ClientUtil.java
@@ -5,7 +5,7 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
 public class S3ClientUtil {
-
+    // Testing code changes 
     public static AmazonS3 getS3Client() {
         return AmazonS3ClientBuilder.standard().withRegion(Regions.DEFAULT_REGION).build();
     }
